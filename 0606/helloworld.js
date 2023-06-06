8e9539e475c3e36203c4178882a905a8d07bdf2a
krui <krui0728@gmail.com>
2023-06-06 11:15:24 +0800

    hello world

---
+++ 0606/helloworld.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: krui krui0728@gmail.com
+ * @Date: 2023-06-06 11:14:13
+ * @LastEditors: krui krui0728@gmail.com
+ * @LastEditTime: 2023-06-06 11:14:26
+ * @FilePath: /node_study/0606/helloworld.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('Hello world');
